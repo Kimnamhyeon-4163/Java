4d55b553153a3ab1fcedabaf04b5f371003de3fc
Kimnamhyeon <namhun4163@icloud.com>
2020-11-10 12:36:05 +0900

    2020/11/10 ch02 수업

---
+++ ch02/src/sub1/HelloJava.java
@@ -0,0 +1,14 @@
+package sub1;
+
+/**
+ * 날짜 : 2020/11/02
+ * 이름 : 김남현
+ * 내용 : ch02 �ڹټ�ġ ���� p31
+ */
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("�ȳ� �ڹ�");
+	}
+}
