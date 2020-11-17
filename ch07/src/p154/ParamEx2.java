75c590cb3dde563ebff8482a01b31c42066bc422
Kimnamhyeon <namhun4163@icloud.com>
2020-11-17 23:08:29 +0900

    2020/11/17 ch07 수정

--- ch07/src/p154/ParamEx2.java
+++ ch07/src/p154/ParamEx2.java
@@ -5,8 +5,8 @@ public class ParamEx2 {
 	public static void main(String[] args) {
 		
 		
-		Param p = new Param();
-		p.add((int)10.5, (int)5.5);
+		//Param p = new Param();
+	//	p.add((int)10.5, (int)5.5);
 		
 	}
 }
