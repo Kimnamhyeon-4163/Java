60517499f17b5f9e6662a58d1ab725e2274c72f7
Kimnamhyeon <namhun4163@icloud.com>
2020-11-27 11:46:09 +0900

    2020/11/27 Test4

---
+++ Test4/src/test02/NotFoundExcepyion.java
@@ -0,0 +1,8 @@
+package test02;
+
+public class NotFoundExcepyion extends Exception{
+
+	public NotFoundExcepyion() {
+		super("해당하는 숫자를 찾을 수 없습니다.");
+	}
+}
---
+++ Test4/src/test07/JavaTest7.java
@@ -0,0 +1,39 @@
+package test07;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class JavaTest7 {
+	public static void main(String[] args) {
+		printList(createList());
+		
+	}
+	
+	public static void printList(List<Integer> scoreList) {
+		int total = 0;
+		int size = scoreList.size();
+		for (int i = 0; i < size; i++) {
+			int score = scoreList.get(i);
+			total += score;
+			System.out.println(score);
+			if (i == size -1) {
+				System.out.println(" = ");
+			}else {
+				System.out.println(" + ");
+			}
+			System.out.println(total);
+		}
+		
+	}
+	public static List<Integer> createList() {
+		List<Integer>scoreList = new ArrayList<>();
+		Random rand = new Random();
+		for (int i = 1; i <= 10; i++) {
+			int num = rand.nextInt(41)+60;		
+			scoreList.add(num);
+		
+		}return scoreList;
+	}
+
+}
